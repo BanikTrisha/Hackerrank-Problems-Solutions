06c7c3849719397041bb42a08adf3a6fbe0367e8
Sudipta Banik Trisha <43735253+Sudipta-Trisha@users.noreply.github.com>
2019-08-21 22:47:01 +0600

    Add files via upload

---
+++ Algorithm/Implementation/Cut the sticks.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+
+	int arr[n];
+	for(int i=0; i<n; i++)
+	{
+		cin>>arr[i];          
+	}
+
+	sort(arr,arr+n);
+
+	int cnt=0;               
+
+	for(int i=0; i<n; i++)
+	{
+		cout<< n-cnt <<endl;
+		int temp = 1;
+
+		for(int j=i+1; j<n; j++)
+		{                                          
+			if(arr[i]==arr[j])
+				temp++;
+				
+			else {
+									
+				i = j-1;
+				break;
+			}
+			
+				
+		}
+		cnt = cnt + temp;
+		if(cnt==n)
+			 break;
+	}
+	
+	return 0;
+}
