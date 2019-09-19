33ba036c9f4f10b75d138e54e9742fb14a903852
Sudipta Banik Trisha <43735253+Sudipta-Trisha@users.noreply.github.com>
2019-09-20 01:03:05 +0600

    Update Marc's Cakewalk.cpp

--- Algorithm/Greedy/Marc's Cakewalk.cpp
+++ Algorithm/Greedy/Marc's Cakewalk.cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+void solve()
+{
+    ll n;
+    cin>>n;
+    vector<ll>v(n);
+    for(int i=0; i<n; i++){
+        cin>>v[i];
+    }
+
+    ll temp=0;
+    sort(v.begin(),v.end(),greater<int>());
+    for(int i=0; i<n; i++){
+        temp = temp + (v[i]*pow(2,i));
+    }
+     cout<<temp<<endl;
+} 
+
+int main()
+{
+    solve();
+    return 0;
+}
 
